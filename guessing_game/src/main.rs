c6e39062008efdd0ecd8a32288ea1c4e83705cf5
梁文亮 <liangwenliang@corp.netease.com>
2019-03-29 19:02:33 +0800

    init rust project

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,6 +4,7 @@ fn main(){
     println!("Please input your guess.");
     //可变变量，默认不可变
     let mut guess = String::new();
+    //这是个引用，引用默认也是不可变的，这里声明为可变的
     io::stdin().read_line(&mut guess).expect("Failed to read line");
     println!("You guessed: {}", guess);
 
