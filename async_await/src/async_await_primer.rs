e87ba2c91fc3131bf69d7ab5e278b68a30208fd1
梁文亮 <liangwenliang@corp.netease.com>
2020-05-08 18:55:31 +0800

    1.async await implementation

--- async_await/src/async_await_primer.rs
+++ async_await/src/async_await_primer.rs
@@ -101,7 +101,7 @@ mod tests {
                     // N.B. it's possible to check for this using the `Waker::will_wake`
                     // function, but we omit that here to keep things simple.
                     shared_state.waker = Some(cx.waker().clone());
-                    Poll::Pending;
+                    Poll::Pending
                 }
             }
         }
@@ -155,13 +155,67 @@ mod tests {
             // This is just to make `sync_channel` happy, and wouldn't be present in
             // a real executor.
             const MAX_QUEUED_TASKS: usize = 10_000;
-            let (task_sender, ready_queue) = sync_channel(MAX_QUEUED_TASKS);
+            let (task_sender, ready_queue) =
+                sync_channel(MAX_QUEUED_TASKS);
             (Executor { ready_queue }, Spawner { task_sender })
         }
         impl Spawner {
             fn spawn(&self, future: impl Future<Output=()> + 'static + Send) {
                 let future = future.boxed();
+                let task = Arc::new(Task {
+                    future: Mutex::new(Some(future)),
+                    task_sender: self.task_sender.clone(),
+                });
+                self.task_sender.send(task).expect("too many tasks queued");
             }
         }
+        impl ArcWake for Task {
+            fn wake_by_ref(arc_self: &Arc<Self>) {
+                // Implement `wake` by sending this task back onto the task channel
+                // so that it will be polled again by the executor.
+                let cloned = arc_self.clone();
+                arc_self.task_sender.send(cloned).expect("too many tasks queued");
+            }
+        }
+        impl Executor {
+            fn run(&self) {
+                while let Ok(task) = self.ready_queue.recv() {
+                    // Take the future, and if it has not yet completed (is still Some),
+                    // poll it in an attempt to complete it.
+                    let mut future_slot = task.future.lock().unwrap();
+                    // 这里take为啥可以调用？不是Option方法吗？因为MutexGuard 实现了Deref trait所以可以强制
+                    // 解构
+                    if let Some(mut future) = future_slot.take() {
+                        // Create a `LocalWaker` from the task itself
+                        let waker = waker_ref(&task);
+                        let context = &mut Context::from_waker(&*waker);
+                        // `BoxFuture<T>` is a type alias for
+                        // `Pin<Box<dyn Future<Output = T> + Send + 'static>>`.
+                        // We can get a `Pin<&mut dyn Future + Send + 'static>`
+                        // from it by calling the `Pin::as_mut` method.
+                        if let Poll::Pending = future.as_mut().poll(context) {
+                            // We're not done processing the future, so put it
+                            // back in its task to be run again in the future.
+                            *future_slot = Some(future);
+                        }
+                    }
+                }
+            }
+        }
+
+        let (executor, spawner) = new_executor_and_spawner();
+        // Spawn a task to print before and after waiting on a timer.
+        spawner.spawn(async {
+            println!("howdy!");
+            // Wait for our timer future to complete after two seconds.
+            TimerFuture::new(Duration::new(2, 0)).await;
+            println!("done!");
+        });
+        // Drop the spawner so that our executor knows it is finished and won't
+        // receive more incoming tasks to run.
+        drop(spawner);
+        // Run the executor until the task queue is empty.
+        // This will print "howdy!", pause, and then print "done!".
+        executor.run();
     }
 }
