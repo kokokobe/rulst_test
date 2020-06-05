b2f0f922136051d73b603f02f23611e61c48b148
梁文亮 <liangwenliang@corp.netease.com>
2020-06-05 18:51:49 +0800

    1.random two point mouse move

--- mouse_script/src/main.rs
+++ mouse_script/src/main.rs
@@ -7,7 +7,6 @@ use rand::prelude::ThreadRng;
 fn main() {
     let mut enigo = Enigo::new();
     // enigo.key_click(Key::Meta);
-    // paste
     // enigo.mouse_move_to(1000, 800);
     // enigo.mouse_click(MouseButton::Right);
     // enigo.mouse_move_relative(100, 100);
