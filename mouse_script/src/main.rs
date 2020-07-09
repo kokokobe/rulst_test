7fd9e4a4a557f3a51a268f14896a0f37e4afb33a
梁文亮 <liangwenliang@corp.netease.com>
2020-07-09 17:37:36 +0800

    1.extract mouse operate func

--- mouse_script/src/main.rs
+++ mouse_script/src/main.rs
@@ -1,74 +1,38 @@
+mod mouse_operate;
+
 use enigo::*;
-use rand::{thread_rng, Rng};
-use std::{thread};
-use std::time::Duration;
-use rand::prelude::ThreadRng;
+use std::time::{SystemTime, UNIX_EPOCH};
+use mouse_operate::start_end_point_click;
 
 fn main() {
     let mut enigo = Enigo::new();
-    // enigo.key_click(Key::Meta);
-    // enigo.mouse_move_to(1000, 800);
-    // enigo.mouse_click(MouseButton::Right);
-    // enigo.mouse_move_relative(100, 100);
-    let drinking_point = (200, 200);
+    // 喝水确认按钮
+    let drinking_confirm_point = (200, 200);
+    // 挑战关卡按钮
     let fight_point = (1000, 1000);
+    // 关卡
+    let battle_point = (500, 300);
+    // 大跳
+    let all_skip_point = (500, 500);
+    // 大跳确认
+    let all_skip_point_confirm = (300, 300);
+
+    let execute_time_sec = 10;
+    let start_time = SystemTime::now().duration_since(UNIX_EPOCH)
+        .expect(" time went backwards");
     loop {
-        start_end_point_click(fight_point, drinking_point, &mut enigo);
-    }
+        //选关，开始挑战
+        start_end_point_click(battle_point, fight_point, &mut enigo, true);
+        //大跳确认
+        start_end_point_click(all_skip_point, all_skip_point_confirm, &mut enigo, true);
+        // 喝水确认
+        start_end_point_click(all_skip_point_confirm, drinking_confirm_point, &mut enigo, true);
 
-
-    fn get_random_move_point(point: (i32, i32), point2: (i32, i32), mut rng: ThreadRng) -> (i32, i32) {
-        let x1 = point.0;
-        let y1 = point.1;
-        let x2 = point2.0;
-        let y2 = point2.1;
-        let mut x_margin = (x1 - x2).abs();
-        let mut y_margin = (y1 - y2).abs();
-        if x_margin <= 1 {
-            x_margin = 2;
-        }
-        if y_margin <= 1 {
-            y_margin = 2;
-        }
-        let random_x = rng.gen_range(0, x_margin);
-        let random_y = rng.gen_range(0, y_margin);
-        (random_x, random_y)
-    }
-
-    fn start_end_point_click(start: (i32, i32), end: (i32, i32), enigo: &mut Enigo) {
-        enigo.mouse_move_to(start.0, start.1);
-        println!("move to start x:{},y:{}", start.0, start.1);
-        enigo.mouse_click(MouseButton::Left);
-        let mut start_point = (start.0, start.1);
-        let mut rng = thread_rng();
-        loop {
-            let x;
-            let y;
-            let random_point = get_random_move_point(start_point, end, rng);
-            if start_point.0 > end.0 {
-                x = start_point.0 - random_point.0;
-            } else if start_point.0 < end.0 {
-                x = start_point.0 + random_point.0;
-            } else {
-                x = start_point.0;
-            }
-            if start_point.1 > end.1 {
-                y = start_point.1 - random_point.1;
-            } else if start_point.1 < end.1 {
-                y = start_point.1 + random_point.1;
-            } else {
-                y = start_point.1;
-            }
-            enigo.mouse_move_to(x, y);
-            println!("move point x:{},y:{}", x, y);
-            thread::sleep(Duration::from_millis(10));
-            start_point.0 = x;
-            start_point.1 = y;
-            if x == end.0 && y == end.1 {
-                enigo.mouse_click(MouseButton::Left);
-                println!("end ");
-                break;
-            }
+        let end_time = SystemTime::now().duration_since(UNIX_EPOCH)
+            .expect(" time went backwards");
+        if end_time.as_millis() - start_time.as_millis() > 1000 * execute_time_sec {
+            break;
         }
     }
+
 }
