f5706c8a6f700eae3f4cf306a1d908d05bb97ba0
梁文亮 <liangwenliang@corp.netease.com>
2020-09-23 16:42:00 +0800

    1.different config file

--- briliang_exercise/tests/test_hello_cargo_crate.rs
+++ briliang_exercise/tests/test_hello_cargo_crate.rs
@@ -4,4 +4,17 @@
 #[test]
 fn test_briliang_exercise() {
     briliang_exercise::data_type::test();
+    struct Foo {
+        x: i32,
+    }
+
+    fn print_value(x: i32) {
+        println!("{}", x)
+    }
+
+    let mut foo = Foo { x: 42 };
+    let x = &mut foo.x;
+    *x = 13;
+    // -> 42; expected result: 13
+    print_value(foo.x);
 }
