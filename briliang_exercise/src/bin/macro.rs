ed225b6e33359e76cf000fd8625b5093dba5a741
梁文亮 <liangwenliang@corp.netease.com>
2021-03-18 15:58:17 +0800

    1.add num val in rust

--- briliang_exercise/src/bin/macro.rs
+++ briliang_exercise/src/bin/macro.rs
@@ -27,4 +27,18 @@ fn main() {
         fn to_usize(self) -> usize;
         fn from(_i: usize) -> Self;
     }
+    let num  = 42u32;
+    println!("u32 num :{}", num);
+    let num = 0x2A;
+    println!("u32 num :{}", num);
+    let num = 0o106;
+    println!("u32 num :{}", num);
+    let num = 0b1101_1011;
+    println!("u32 num :{}", num);
+    assert_eq!(b'*', 42u8);
+    assert_eq!(-3.14, -3.14f64);
+    assert_eq!('\x2A', '*');
+    println!("unicode 字符：{}", '\u{CA0}');
+    let int_array = [0; 10];
+    println!("int array {:?}", int_array);
 }
